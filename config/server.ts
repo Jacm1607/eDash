2579444151eb08f25dd8468e3c4be4df4b9c492c
Jose <chavarriaj84@gmail.com>
2023-07-01 14:44:37 +0000

    config server url to email reset

--- config/server.ts
+++ config/server.ts
@@ -7,4 +7,5 @@ export default ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  url: env('', 'https://www.dashboard.hauscenter.com.bo'),
 });
